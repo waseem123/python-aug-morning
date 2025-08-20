aa42112d7aad5fd6f52aab4945c558d1619fccdc
WASEEM ATTAR <waseem_6957@yahoo.com>
2025-08-20 15:28:29 +0530

    Add files via upload

---
+++ print-statements.py
@@ -0,0 +1,24 @@
+print("Welcome to python tutorials")
+print('This is a new line')
+print('This is Waseem sir\'s class')
+print('I am learning "Basics of Python"')
+print("_________________")
+
+a = 50
+b = 20
+c = a * b
+print('Value of a variable is',a)
+print(a,b,c)
+print("___________________")
+
+name = 'Waseem'
+surname = 'Attar'
+print('My name is',name,surname,'.')
+print('My name is '+name+' '+surname)
+print('Multiplication of a and b is c')
+print('Multiplication of '+str(a)+' and '+str(b)+' is '+str(c))
+print("_______________________")
+
+print(f'Multiplication of {a} and {b} is {c}')
+print(f'My full name is {name} {surname}.')
+print(f'Subtraction of {a} and {b} is {a-b}')
---
+++ variables.py
@@ -0,0 +1,37 @@
+# 1. Variable name starts with character
+# 2. Variable names cannot have space or line gaps
+# 3. Variable names cannot have any special character except underscore(_)
+# 4. Variable names cannot have any keyword or system defined word
+# 5. Variables are case sensitive
+# 6. Its a good practice to define the meaningful names of variables
+
+x1y = 627
+print(x1y)
+
+_a = 'Mumbai'
+print(_a)
+
+# print = 35
+# print(print)
+
+demo = 123
+DEMO = 246
+Demo = 789
+dEMO = 456
+print(DEMO)
+print(dEMO)
+
+firstnumber = 23
+secondnumber = 25
+division = firstnumber / secondnumber
+print(division)
+
+student = {
+    'name':'Alex',
+    'city':'Paris',
+    'marks':85
+}
+
+print(student)
+print(student['name'])
+print(student['city'])
